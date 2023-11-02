17ed77d9f4ccd5796b99401a04da8363de2d679d
martinfarro <martin.farro.martinez@gmail.com>
2023-11-02 18:10:50 -0500

    end commit

--- src/modules/orders/orders.route.js
+++ src/modules/orders/orders.route.js
@@ -1,7 +1,4 @@
 import express from 'express';
-import { validExistOrder } from './orders.middleware.js'
-import { validExistReview } from '../review/review.middleware.js'
-import { protectAccountOwner } from '../users/auth.middleware.js'
 
 export const router = express.Router()
 
